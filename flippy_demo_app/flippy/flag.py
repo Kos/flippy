2011500ed4f497161ac1b17ec43f36c25a70e0e3
Tomasz Wesołowski <kosashi@gmail.com>
2019-04-14 00:12:15 +0200

    Add more tests for get_state_for_request

--- flippy_demo_app/flippy/flag.py
+++ flippy_demo_app/flippy/flag.py
@@ -11,7 +11,7 @@ class Flag:
         # -> don't import models at import time
         from .models import Rollout
 
-        for rollout in Rollout.objects.all():  # TODO
+        for rollout in Rollout.objects.filter(flag_name=self.name):
             subject: Subject = rollout.subject_instance
             identifier = subject.get_subject_identifier_for_request(request)
             score = identifier.get_flag_score(self.name)
--- flippy_demo_app/flippy/flag_test.py
+++ flippy_demo_app/flippy/flag_test.py
@@ -21,12 +21,24 @@ def test_flag_true_default_value():
     assert f.get_state_for_request(request_factory()) is True
 
 
-def test_flag_uses_rollout():
+@pytest.mark.parametrize(
+    "percentage,result", [(0, False), (10, False), (20, True), (100, True)]
+)
+def test_flag_uses_rollout(percentage, result):
     f = Flag("hello")
-    assert f.get_state_for_request(request_factory()) is False
     Rollout.objects.create(
         flag_name=f.name,
+        enable_percentage=percentage,
+        subject="flippy.subject.IpAddressSubject",
+    )
+    assert f.get_state_for_request(request_factory()) is result
+
+
+def test_flag_uses_correct_rollout():
+    f = Flag("hello")
+    Rollout.objects.create(
+        flag_name="hello2",
         enable_percentage=100,
         subject="flippy.subject.IpAddressSubject",
     )
-    assert f.get_state_for_request(request_factory()) is True
+    assert f.get_state_for_request(request_factory()) is False
