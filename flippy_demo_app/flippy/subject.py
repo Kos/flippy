cc02ab8b5263f7bbbbc8621c613906a4e6ced3a1
Tomasz Wesołowski <kosashi@gmail.com>
2019-04-13 21:49:24 +0200

    Add Subject and IpAddressSubject

--- flippy_demo_app/flippy/subject.py
+++ flippy_demo_app/flippy/subject.py
@@ -1,7 +1,30 @@
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from django.http import HttpRequest
 
 
 @dataclass
 class SubjectIdentifier:
     subject_class: str
     subject_id: str
+
+
+class Subject(ABC):
+    @abstractmethod
+    def get_subject_identifier_for_request(
+        self, request: HttpRequest
+    ) -> SubjectIdentifier:
+        ...
+
+    @property
+    def subject_class(self):
+        cls = type(self)
+        return cls.__module__ + "." + cls.__name__
+
+
+class IpAddressSubject(Subject):
+    def get_subject_identifier_for_request(
+        self, request: HttpRequest
+    ) -> SubjectIdentifier:
+        ip = request.META["REMOTE_ADDR"]
+        return SubjectIdentifier(self.subject_class, ip)
--- flippy_demo_app/flippy/subject_test.py
+++ flippy_demo_app/flippy/subject_test.py
@@ -1,7 +1,25 @@
-from .subject import SubjectIdentifier
+from .subject import IpAddressSubject, SubjectIdentifier
+from mockito import mock
+from django.http import HttpRequest
+import pytest
 
 
 def test_subject_identifier():
     identifier = SubjectIdentifier(subject_class="hello", subject_id="123")
     assert identifier.subject_class == "hello"
     assert identifier.subject_id == "123"
+
+
+def test_ip_address_subject(request):
+    assert IpAddressSubject().get_subject_identifier_for_request(
+        request
+    ) == SubjectIdentifier("flippy.subject.IpAddressSubject", example_ip)
+
+
+@pytest.fixture
+def request():
+    spec = {"META": {"REMOTE_ADDR": example_ip}}
+    return mock(spec, HttpRequest)
+
+
+example_ip = "10.1.2.3"
