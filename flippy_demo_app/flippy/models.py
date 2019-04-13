933caf460f2090c99cb79efa6856beeaff8d2f7c
Tomasz Wesołowski <kosashi@gmail.com>
2019-04-13 22:58:57 +0200

    Add the Rollout model

--- flippy_demo_app/flippy/models.py
+++ flippy_demo_app/flippy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from .subject import Subject
 
-# Create your models here.
+
+class Rollout(models.Model):
+    """A Rollout is what happens when someone changes the value of a flag."""
+
+    flag_name: str = models.CharField(max_length=64)
+    enable_percentage: float = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    subject: str = models.TextField(choices=Subject.choices())
--- flippy_demo_app/flippy_demo_app/settings.py
+++ flippy_demo_app/flippy_demo_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "flippy",
     "flippy_demo_app",
 ]
 
