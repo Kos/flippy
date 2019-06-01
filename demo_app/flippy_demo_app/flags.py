a64e2c19830ee1cd12acd2594d626fcc62464f9f
Tomasz Wesołowski <kosashi@gmail.com>
2019-06-01 20:55:03 +0200

    Update Demo app

--- demo_app/flippy_demo_app/flags.py
+++ demo_app/flippy_demo_app/flags.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 from flippy.flag import TypedFlag
 
-enable_weather = Flag("enable_weather", "Weather widget")
-enable_cats = Flag("enable_cats", "Cats widget", default=settings.DEBUG)
-enable_sudoku = TypedFlag[User]("enable_sudoku", "Sudoku")
+enable_weather: Flag = Flag("enable_weather", "Weather widget")
+enable_cats: Flag = Flag("enable_cats", "Cats widget", default=settings.DEBUG)
+enable_sudoku: TypedFlag[User] = TypedFlag[User]("enable_sudoku", "Sudoku")
--- demo_app/flippy_demo_app/views.py
+++ demo_app/flippy_demo_app/views.py
@@ -1,16 +1,27 @@
+from django.contrib.auth.models import User
 from django.template.response import TemplateResponse
 
 from .flags import enable_weather, enable_cats, enable_sudoku
 
 
 def index(request):
+    if request.user.is_authenticated:
+        show_sudoku = should_show_sudoku(request.user)
+    else:
+        show_sudoku = False
+
+    # Any flag can be queried with a request.
     return TemplateResponse(
         request,
         "index.html",
         {
             "show_weather": enable_weather.get_state_for_request(request),
             "show_cat": enable_cats.get_state_for_request(request),
-            # TODO extract method
-            "show_sudoku": enable_sudoku.get_state_for_object(request.user),
+            "show_sudoku": show_sudoku,
         },
     )
+
+
+def should_show_sudoku(user: User):
+    # TypedFlags can be queried anywhere in the code, even without a request:
+    return enable_sudoku.get_state_for_object(user)
